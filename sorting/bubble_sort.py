cca026cd32f59811ff2c7472e8c25ca889e86f67
Manish Lama <lmanish931@gmail.com>
2024-08-21 08:09:14 +0545

    bubble sort

---
+++ sorting/bubble_sort.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+    n = len(arr)
+    
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if (arr[j] > arr[j+1]):
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+                
+                
+    return arr
+
+arrayto = [1,6,5]
+
+a = bubble_sort(arrayto)
+
+print(a)
