5f292e96e1958cb34b8c76b0a10b684a8ba77742
Manish Lama <lmanish931@gmail.com>
2024-08-23 16:52:56 +0545

    selection sort complete

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -2,7 +2,13 @@ def selection_sort(arr):
     n = len(arr)
     for i in range(n-1):
         min_index = i
-        for j in range(i+1,n-1):
-            if (arr[i] < arr[j]):
-                min_index
-            
+        for j in range(i+1,n):
+            if (arr[j] < arr[min_index]):
+                min_index = j
+        arr[i],arr[min_index] = arr[min_index],arr[i]
+    return arr
+
+
+array_to_sort = [1,4,5,6,3]    
+sorted_array  = selection_sort(array_to_sort)
+print(sorted_array)
