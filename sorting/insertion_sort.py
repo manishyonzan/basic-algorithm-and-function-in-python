60a5dd35f49890325740b9db17d67a67c86b88ff
Manish Lama <lmanish931@gmail.com>
2024-08-25 21:10:36 +0545

    insertion sort added

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -1,6 +1,10 @@
 def insertion_sort(arr):
     for i in range(1,len(arr)):
-        value = arr[i]
-        for j in range(0,i+1):
-            if (arr[j]<arr[i]):
-                return
+        j = i
+        while j > 0 and arr[j-1] > arr[j]:
+            arr[j-1],arr[j] = arr[j],arr[j-1]
+            j = j-1
+    return arr
+        
+arr2  = insertion_sort([2,3,7,1,16])
+print(arr2)
