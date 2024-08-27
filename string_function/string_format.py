0833296cabc5e6cb2eac6b9703a3c14b0f1a429e
Manish Lama <lmanish931@gmail.com>
2024-08-27 22:40:54 +0545

    string functions using class

--- string_function/string_format.py
+++ string_function/string_format.py
@@ -3,8 +3,8 @@ print(ram)
 
 hari = "hello {}".format("hari")
 print(hari)
-c = "{} {} {}".format("first","second","third")
-print(c)
+a = "{} {} {}".format("first","second","third")
+print(a)
 
 d= "{2} {0} {1}".format("first", "second","third")
 print(d)
@@ -38,3 +38,26 @@ print(g)
 
 h = "{d[name]}".format(d=example_dict)
 print(h)
+
+
+
+
+class container():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def __repr__(self) -> str:
+        return "repr of container"
+    def __str__(self) -> str:
+        return "Str of container"
+    
+c = container(5,6)
+print("{}".format(c))
+print("{0.x}".format(c))
+print("{c.y}".format(c=c))
+print("{!r}".format(c))
+print("{!s}".format(c))
+print("{!a}".format(c))
+
+print("{0.x!r}".format(c))
+print("{0.x!s}".format(c))
