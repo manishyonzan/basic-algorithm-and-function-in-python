5cbef720a6dfd0005dfdc70a676235d5fe4ef5d6
Manish Lama <lmanish931@gmail.com>
2025-06-14 23:59:41 +0545

    dictonaries.py complete

--- w3schools-python-basic/11dictionaries.py
+++ w3schools-python-basic/11dictionaries.py
@@ -249,3 +249,66 @@ print(mydict) #{'brand': 'Ford', 'model': 'Mustang', 'year': 1964}
 
 
 
+
+
+# Nested dictionaries
+
+
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+print(myfamily) #{'child1': {'name': 'Emil', 'year': 2004}, 'child2': {'name': 'Tobias', 'year': 2007}, 'child3': {'name': 'Linus', 'year': 2011}}
+
+child1 = {
+  "name" : "Emil",
+  "year" : 2004
+}
+child2 = {
+  "name" : "Tobias",
+  "year" : 2007
+}
+child3 = {
+  "name" : "Linus",
+  "year" : 2011
+}
+
+myfamily = {
+  "child1" : child1,
+  "child2" : child2,
+  "child3" : child3
+}
+
+
+print(myfamily)  #{'child1': {'name': 'Emil', 'year': 2004}, 'child2': {'name': 'Tobias', 'year': 2007}, 'child3': {'name': 'Linus', 'year': 2011}}
+
+
+
+print(myfamily["child2"]["name"])   #Tobias
+
+
+for x, obj in myfamily.items():
+  print(x)
+
+  for y in obj:
+    print(y + ':', obj[y])
+    
+# child1
+# name: Emil
+# year: 2004
+# child2
+# name: Tobias
+# year: 2007
+# child3
+# name: Linus
+# year: 2011
