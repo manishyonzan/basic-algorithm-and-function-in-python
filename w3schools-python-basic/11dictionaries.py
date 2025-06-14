5cb8f1f606f636964fc38d32e2d124078f5ec266
Manish Lama <lmanish931@gmail.com>
2025-06-14 23:55:08 +0545

    dictionaries till copu

--- w3schools-python-basic/11dictionaries.py
+++ w3schools-python-basic/11dictionaries.py
@@ -148,3 +148,104 @@ thisdict.update({"color": "blue"})
 print(thisdict)  #{'brand': 'Ford', 'model': 'Mustang', 'year': 1964, 'color': 'blue'}
 
 
+# remove from a dict
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict) #{'brand': 'Ford', 'year': 1964}
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.popitem()
+print(thisdict) #{'brand': 'Ford', 'model': 'Mustang'}
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict["model"]
+print(thisdict)  #{'brand': 'Ford', 'year': 1964}
+
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict
+# print(thisdict)   #NameError: name 'thisdict' is not defined
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.clear()
+print(thisdict)  #{}
+
+
+
+# loop dictionary
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+for x in thisdict:
+  print(x) #print keys
+  
+  
+for x in thisdict:
+    print(thisdict[x]) # print values
+    
+    
+    
+for x in thisdict.values():
+  print(x, end="") #FordMustang1964
+  
+print("\n")
+for x in thisdict.keys():
+  print(x, end="") #brandmodelyear
+  
+print("\n")
+for x, y in thisdict.items():
+  print(x, y)
+# brand Ford
+# model Mustang
+# year 1964  
+
+# Copy a Dictionary
+# You cannot copy a dictionary simply by typing dict2 = dict1, because: dict2 will only be a reference to dict1, and changes made in dict1 will automatically also be made in dict2.
+
+# There are ways to make a copy, one way is to use the built-in Dictionary method copy().
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = thisdict.copy()
+thisdict2 = thisdict
+thisdict2['brand'] = "Honda"
+
+print(thisdict) #{'brand': 'Honda', 'model': 'Mustang', 'year': 1964}
+print(mydict) #{'brand': 'Ford', 'model': 'Mustang', 'year': 1964}
+
+
+
