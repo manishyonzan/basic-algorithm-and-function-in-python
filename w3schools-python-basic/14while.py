45ecd150208a8c905eaa6e79364b91582f407086
Manish Lama <lmanish931@gmail.com>
2025-06-18 23:26:31 +0545

    while loop

---
+++ w3schools-python-basic/14while.py
@@ -0,0 +1,33 @@
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+print("next")
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+  
+print("next")
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+
+print('next')
+
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
