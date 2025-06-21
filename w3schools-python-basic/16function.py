a8a64409e802aa900d97d8dbad7454df81392db5
Manish Lama <lmanish931@gmail.com>
2025-06-21 23:58:12 +0545

    function added

--- w3schools-python-basic/16function.py
+++ w3schools-python-basic/16function.py
@@ -18,7 +18,8 @@ my_funciton("messi")
 
 
 # to pass a list kind of data
-
+# If you do not know how many arguments that will be passed into your function, add a * before the parameter name in the function definition.
+# This way the function will receive a tuple of arguments, and can access the items accordingly:
 def my_function(*kids):
   print("The youngest child is " + kids[2])
 
@@ -55,5 +56,84 @@ def my_function(**kid):
   print("His last name is " + kid["lname"])
 
 my_function(fname = "Tobias", lname = "Refsnes")
+
+
+
+
+# default parameter
+
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function() #I am from Norway
+my_function("Brazil") #I am from Brazil
+
+
+def my_funciton(food):
+    for x in food:
+        print(x)
+        
+        
+fruits = ['apple', 'banana', 'pineapple']
+
+my_funciton(fruits)
+
+
+
+# return values
+
+def my_function(x):
+  return 5 * x
+
+print(my_function(3)) # 15
+
+
+
+# Positional-Only Arguments
+# You can specify that a function can have ONLY positional arguments, or ONLY keyword arguments.
+
+# To specify that a function can have only positional arguments, add , / after the arguments:
+
+
+def my_function(x,/):
+# def my_function(x, /,y): we can also use this
+
+  print(x)
+
+my_function(3)
+
+
+# Without the , / you are actually allowed to use keyword arguments even if the function expects positional arguments:
+    
+def my_function(x):
+  print(x)
+
+my_function(x = 3)
+
+
+def my_function(*, x):
+  print(x)
+
+my_function(x = 4)
+
+
+def my_function(a, b, /, *, c, d):
+  print(a + b + c + d)
+
+my_function(5, 6, c = 7, d = 8)
+
+
+
+
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("Recursion Example Results:")
+tri_recursion(6)
 
 
