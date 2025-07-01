f9a31d7e48dd19ea9361264d33035f6c8e6d0175
Manish Lama <lmanish931@gmail.com>
2025-07-02 00:15:27 +0545

    longest substring

--- 15patterns-for-dsa-problems/sliding_pattern.py
+++ 15patterns-for-dsa-problems/sliding_pattern.py
@@ -35,3 +35,37 @@ class Solution(object):
 a = Solution()
 b = a.findMaxAverage([1,12,-5,-6,50,3],4)
 print(b)
+
+
+
+# 3. Longest Substring Without Repeating Characters
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s) < 1:
+            return 0
+        
+        longest_substring = 1
+        substring = s[0]
+        print("\n")
+        for i in range(1, len(s)):
+            print(s[i])
+            if s[i] in substring:
+                substring = substring[substring.index(s[i]) + 1:] + s[i]
+            else:
+                substring = substring + s[i]
+            if len(substring) > longest_substring:
+                longest_substring = len(substring)
+                
+        return longest_substring
+            
+            
+        
+
+a = Solution()
+m = a.lengthOfLongestSubstring("abcdabc")
+print(m)
