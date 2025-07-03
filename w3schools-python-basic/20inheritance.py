83ba434e84f6293408028f508a073ef301d33621
Manish Lama <lmanish931@gmail.com>
2025-07-03 23:43:20 +0545

    inheritance done from w3schools

--- w3schools-python-basic/20inheritance.py
+++ w3schools-python-basic/20inheritance.py
@@ -18,3 +18,32 @@ student1 = Student("Hari", "bahadur")
 student1.printname() #Hari bahadur
 
 
+# Add the __init__() Function
+# So far we have created a child class that inherits the properties and methods from its parent.
+# We want to add the __init__() function to the child class (instead of the pass keyword).
+
+class Student(Person):
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+        
+        # to keep the inheritance of the parent's __init__() function, add a call to the parent's __init__() function:
+        # Person.__init__(self, fname, lname)
+        
+person2 = Student("j", "k")
+person2.printname()
+
+
+
+class Student(Person):
+    def __init__(self, fname, lname, year):
+        super().__init__(fname, lname)
+        self.graduationyear = year
+    def welcome(self):
+        print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+
+
+x = Student("Mike", "Olsen", 2019)
+
+x.printname()
+x.welcome()
