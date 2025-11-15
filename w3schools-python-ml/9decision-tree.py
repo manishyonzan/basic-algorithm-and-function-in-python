0a92271b15f854a9911e0d696690f05b1fd0d8f8
Manish Lama <lmanish931@gmail.com>
2025-11-15 23:06:17 +0545

    decision tree

---
+++ w3schools-python-ml/9decision-tree.py
@@ -0,0 +1,42 @@
+# import pandas as pd
+
+# df = pd.read_csv("csv/data2.csv")
+
+# d = {'UK': 0, 'USA': 1, 'N': 2}
+# df['Nationality'] = df['Nationality'].map(d)
+# d = {'YES': 1, 'NO': 0}
+# df['Go'] = df['Go'].map(d)
+
+# print(df)
+
+
+# features = ['Age', 'Experience', 'Rank', 'Nationality']
+
+# X = df[features]
+# y = df['Go']
+
+# print(X)
+# print(y)
+
+
+import pandas as pd
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("csv/data2.csv")
+
+d = {'UK': 0, 'USA': 1, 'N': 2}
+df['Nationality'] = df['Nationality'].map(d)
+d = {'YES': 1, 'NO': 0}
+df['Go'] = df['Go'].map(d)
+
+features = ['Age', 'Experience', 'Rank', 'Nationality']
+
+X = df[features]
+y = df['Go']
+
+dtree = DecisionTreeClassifier()
+dtree = dtree.fit(X, y)
+
+tree.plot_tree(dtree, feature_names=features)
