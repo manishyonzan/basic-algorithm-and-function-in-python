ad415a021554b551f59f8d90a913a9fd1988a191
Manish Lama <lmanish931@gmail.com>
2026-02-05 23:59:14 +0545

    problem statement added linked list middle problem

--- practice/dsa-leetcode-freecodecamp/5Linked_list_middle.py
+++ practice/dsa-leetcode-freecodecamp/5Linked_list_middle.py
@@ -1,5 +1,7 @@
 # finding the middle node of the linked list
 
+# problem statement: return the middle of the node and if the linked list have even number return the second middle one
+
 class Node:
     def __init__(self, val, next):
         self.val = val
