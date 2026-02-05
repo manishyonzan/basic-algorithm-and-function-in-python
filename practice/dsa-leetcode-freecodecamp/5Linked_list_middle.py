4703772f5d589b284215b2ed2d863e75a399c1df
Manish Lama <lmanish931@gmail.com>
2026-02-05 23:57:54 +0545

    linked list middle problem initiated

---
+++ practice/dsa-leetcode-freecodecamp/5Linked_list_middle.py
@@ -0,0 +1,11 @@
+# finding the middle node of the linked list
+
+class Node:
+    def __init__(self, val, next):
+        self.val = val
+        self.next = next
+
+
+
+def middle_of_linked_list(head:Node) -> int:
+    pass
