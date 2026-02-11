1bfdfafba682054ca42e64bb7f5dcf8458673683
Manish Lama <lmanish931@gmail.com>
2026-02-12 00:10:52 +0545

    int

--- w3schools-python-basic/26list_duplicates.py
+++ w3schools-python-basic/26list_duplicates.py
@@ -21,3 +21,21 @@ print(text)
 
 def reverse_str(x):
     return x[::-1]
+
+
+mytxt = reverse_str("my string is this and what is this")
+print(mytxt)
+
+
+
+# print two number
+
+x = 5
+y = 9
+
+print(x + y)
+
+x   = input("enter a value for x")
+y  = input("enter a value for y")
+
+print(int(x) + int(y))
