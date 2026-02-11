d41cc4b8f3c7bbf83a5b4c6a3f84f15f78cc1c25
Manish Lama <lmanish931@gmail.com>
2026-02-11 23:59:32 +0545

    reverse string added

--- w3schools-python-basic/26list_duplicates.py
+++ w3schools-python-basic/26list_duplicates.py
@@ -2,3 +2,22 @@ mylist = ["a", "b", "a", "c", "c"]
 mylist = list(dict.fromkeys(mylist))
 
 print(mylist)
+
+
+
+# function where you can send list and get them without duplicates 
+
+def myfunction(x):
+    return  list(dict.fromkeys(x))
+
+mylist = myfunction([*mylist, "e"])
+print(mylist)
+
+
+# reverse a string
+text = "Hello World" [::-1]
+print(text)
+
+
+def reverse_str(x):
+    return x[::-1]
