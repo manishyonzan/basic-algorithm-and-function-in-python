f4768815786b5790f80aa93419e58a5e00fe8512
Manish Lama <lmanish931@gmail.com>
2026-02-15 01:15:20 +0545

    commit for use of class queue

--- w3schools-python-basic/DSA/1Queue.py
+++ w3schools-python-basic/DSA/1Queue.py
@@ -21,3 +21,53 @@ print("isEmpty: ", isEmpty)
 
 # Size
 print("Size: ", len(queue))
+
+
+
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+        
+    def enqueue(self,value):
+        self.queue.append(value)
+        
+    def dequeue(self):
+        if self.isEmpty():
+            return "queue is empty"
+        return self.queue.pop(0)
+    
+    def peek(self):
+        if self.isEmpty():
+            return "queue is empty"
+        return self.queue[0]
+    
+    def isEmpty(self):
+        return len(self.queue) == 0
+    
+    
+    
+classQueue = Queue()
+
+classQueue.enqueue(1)
+classQueue.enqueue(2)
+classQueue.enqueue(3)
+classQueue.enqueue(4)
+classQueue.enqueue(5)
+classQueue.enqueue(6)
+
+classQueue.peek()
+
+classQueue.dequeue()
+
+print("Peek",classQueue.peek())
+print("Is empty ?",classQueue.isEmpty())
+
+classQueue.dequeue()
+classQueue.dequeue()
+classQueue.dequeue()
+classQueue.dequeue()
+classQueue.dequeue()
+
+
+print("Is empty? ",classQueue.isEmpty())
